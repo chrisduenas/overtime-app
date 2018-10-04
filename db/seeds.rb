078f32af9997e1c3f9f7710790a32952d9f78afa
Christopher Duenas <chrisduenasonline@gmail.com>
2018-10-04 15:58:09 -0400

    Brought in live data for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,8 @@ AdminUser.create(email: "admin@test.com",
 puts "1 Admin user created"
 
 100.times do |post|
-    Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+    Post.create!(date: Date.today, rationale: "#{post} You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man.
+    ", user_id: @user.id, overtime_request: 2.5)
 end
 
 100.times do |audit_log|
