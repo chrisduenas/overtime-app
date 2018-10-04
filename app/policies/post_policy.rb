e4e596b930470ba497a4a40b6df63876f8b6fca6
Christopher Duenas <chrisduenasonline@gmail.com>
2018-10-04 16:18:35 -0400

    Built in approval and review buttons for admin review homepage

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -4,17 +4,21 @@ class PostPolicy < ApplicationPolicy
         return true if user_or_admin && !post_approved?
     end
 
+    def approve?
+        admin?
+    end
+
     private
 
-    def user_or_admin
-        record.user_id == user.id || admin?
-    end
+        def user_or_admin
+            record.user_id == user.id || admin?
+        end
 
-    def admin?
-        admin_types.include?(user.type)
-    end
+        def admin?
+            admin_types.include?(user.type)
+        end
 
-    def post_approved?
-        record.approved?
-    end
+        def post_approved?
+            record.approved?
+        end
 end
