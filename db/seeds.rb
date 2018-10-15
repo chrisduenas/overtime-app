d207024daa06772d06d71681f4c50da53380c9cd
Christopher Duenas <chrisduenasonline@gmail.com>
2018-10-15 14:12:51 -0400

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,13 +16,15 @@ AdminUser.create(email: "admin@test.com",
 
 puts "1 Admin user created"
 
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+
+puts "3 audit logs have been created"
+
 100.times do |post|
     Post.create!(date: Date.today, rationale: "#{post} You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man.
     ", user_id: @user.id, overtime_request: 2.5)
 end
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
-
-puts "3 audit logs and posts have been created"
+puts "100 Posts have been created"
